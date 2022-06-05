7d84b658a8442fc3880c08ff4ef82f3b5c6973ff
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-05 15:52:57 -0400

    Fix: reorganize character_set initialization

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,7 +3,12 @@ class Enigma
   attr_reader :character_set
 
   def initialize
-    @character_set = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    @character_set = ["a", "b", "c", "d", "e",
+                      "f", "g", "h", "i", "j",
+                      "k", "l", "m", "n", "o",
+                      "p", "q", "r", "s", "t",
+                      "u", "v", "w", "x", "y",
+                      "z", " "]
   end
 
   def a_key(string)
