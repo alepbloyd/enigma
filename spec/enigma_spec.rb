b65e91fdb68e38bc5a191af7b14293a7fef9a226
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-05 15:12:00 -0400

    Test: return hash of encryption, key, date

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Enigma do
     enigma = Enigma.new
   end
 
+  it 'returns hash of encryption, key, and date' do
+    enigma = Enigma.new
+
+    expect(enigma.encrypt("hello world", "02715", "040895")).to be_instance_of(Hash)
+    
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq({
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+   })
+  end
+
 end
