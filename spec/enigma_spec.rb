4f107e620b3eb8c0ef451c8e3e58855d819c609c
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-05 15:20:19 -0400

    Test: return 'B' key

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -16,6 +16,10 @@ RSpec.describe Enigma do
     expect(@enigma.a_key('02715')).to eq('02')
   end
 
+  it 'returns "B" key from number' do
+    expect(@enigma.b_key('02715')).to eq('27')
+  end
+
   xit 'returns hash of encryption, key, and date' do
 
     expect(enigma.encrypt("hello world", "02715", "040895")).to be_instance_of(Hash)
