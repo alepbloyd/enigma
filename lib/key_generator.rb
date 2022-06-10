4ff82308876cd1fe8a7a68bcf2083830fba1f1b2
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-10 11:43:10 -0400

    Feat: input_hash_first_5

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'digest'
 
 class KeyGenerator
 
@@ -7,23 +8,26 @@ class KeyGenerator
   #if it's an integer of five digits, use that instead of hash
 
   attr_reader :input,
-              :input_data_type
+              :input_data_type,
+              :input_hash
 
   def initialize(input)
     @input = input
     @input_data_type = input.class
-  end
-
-  def length
-    if @input_data_type == Integer
-      @input
-    elsif @input_data_type == String
-      @input.length
-    end
+    @input_hash = Digest::SHA1.base64digest(input.to_s)
+    @input_hash_first_5 = ""
   end
 
   def random_digit
     rand(9)
+  end
+
+  def hash_base64
+    @input_hash = Digest::SHA1.base64digest(@input)
+  end
+
+  def input_hash_first_5
+    @input_hash_first_5 = @input_hash[0,5]
   end
 
 end
