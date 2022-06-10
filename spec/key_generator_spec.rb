5d689e18c0fbfcc4d72ab96209a2beb31b21828c
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-10 00:23:45 -0400

    Test: return length of integer inputs

--- spec/key_generator_spec.rb
+++ spec/key_generator_spec.rb
@@ -7,21 +7,28 @@ require "pry"
 RSpec.describe KeyGenerator do
 
   before(:each) do
-    @key_generator = KeyGenerator.new(5)
+    @key_generator1 = KeyGenerator.new(5)
+    @key_generator2 = KeyGenerator.new("Beefaroni")
   end
 
   it 'exists' do
-    expect(@key_generator).to be_instance_of(KeyGenerator)
+    expect(@key_generator1).to be_instance_of(KeyGenerator)
   end
 
-  it 'returns length' do
-    expect(@key_generator.length).to eq(5)
+  it 'returns input_type' do
+    expect(@key_generator1.input_type).to eq(Integer)
+
+    expect(@key_generator2.input_type).to eq(String)
   end
 
-  it 'generates a digit' do
-    allow(@key_generator).to receive(:generate_digit).and_return(8)
-
-    expect(@key_generator.generate_digit).to eq(8)
+  it 'returns length of integer inputs' do
+    expect(@key_generator1.length).to eq(5)
   end
+
+  # it 'generates a digit' do
+  #   allow(@key_generator).to receive(:generate_digit).and_return(8)
+  #
+  #   expect(@key_generator.generate_digit).to eq(8)
+  # end
 
 end
