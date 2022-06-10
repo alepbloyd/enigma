a570b477c77930c360b5c8e7019d5ac950b3a76a
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-10 12:22:45 -0400

    Feat: generate random five digit string

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -5,24 +5,33 @@ require 'digest'
 
 class KeyGenerator
 
-  #read an input
-  #if it's a string, convert it to a five digit hash (not random)
-  #if it's an integer of five digits, use that instead of hash
-
   attr_reader :input,
               :input_data_type,
-              :input_hash,
-              :input_hash_first_5
+              :input_hash
 
   def initialize(input = nil)
     @input = input
     @input_data_type = input.class
     @input_hash = Digest::SHA1.base64digest(input.to_s)
-    @input_hash_first_5 = ""
+  end
+
+  def generate_five_digit_key
+    if @input == nil
+      generate_random_five_digit_key
+    elsif @input.five_digit_string?
+      @input
+    else
+      key_from_password
+    end
   end
 
   def random_digit
     rand(9)
+  end
+
+  def generate_random_five_digit_key
+    return_string = ""
+    5.times {return_string += random_digit.to_s}
   end
 
   def hash_base64
@@ -30,7 +39,7 @@ class KeyGenerator
   end
 
   def input_hash_first_5
-    @input_hash_first_5 = @input_hash[0,5]
+    @input_hash[0,5]
   end
 
   def ord_numbers_for_first_five
