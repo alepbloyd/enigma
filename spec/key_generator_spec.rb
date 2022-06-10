65da01c0cdbc3109cc049226a80aa79d87e2890d
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-10 01:24:14 -0400

    Test: returns sha1_base64_digest hash for string input

--- spec/key_generator_spec.rb
+++ spec/key_generator_spec.rb
@@ -2,12 +2,14 @@ require 'simplecov'
 SimpleCov.start
 require './lib/enigma'
 require './lib/key_generator'
+require 'digest'
+
 require "pry"
 
 RSpec.describe KeyGenerator do
 
   before(:each) do
-    @key_generator1 = KeyGenerator.new(5)
+    @key_generator1 = KeyGenerator.new(27150)
     @key_generator2 = KeyGenerator.new("Beefaroni")
   end
 
@@ -15,14 +17,16 @@ RSpec.describe KeyGenerator do
     expect(@key_generator1).to be_instance_of(KeyGenerator)
   end
 
-  it 'returns input_type' do
+  it 'returns integer if input is integer' do
     expect(@key_generator1.input_data_type).to be Integer
+  end
 
+  it 'returns string if input is string' do
     expect(@key_generator2.input_data_type).to be String
   end
 
   it 'returns length of integer inputs' do
-    expect(@key_generator1.length).to eq(5)
+    expect(@key_generator1.length).to eq(27150)
   end
 
   it 'returns length of string inputs' do
@@ -33,6 +37,11 @@ RSpec.describe KeyGenerator do
     allow(@key_generator1).to receive(:random_digit).and_return(8)
 
     expect(@key_generator1.random_digit).to eq(8)
+  end
+
+  it 'returns SHA1_base64_digest hash for string' do
+    # binding.pry
+    expect(@key_generator2.hash_base64).to eq("q84k4HpPRNqSDTesNapvEOhObHI=")
   end
 
 end
