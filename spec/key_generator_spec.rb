ee5cf00e491f1e66d3a00aa058733b275baf7ef1
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-10 11:42:53 -0400

    Test: returns first five characters of base64 hash for string

--- spec/key_generator_spec.rb
+++ spec/key_generator_spec.rb
@@ -25,14 +25,6 @@ RSpec.describe KeyGenerator do
     expect(@key_generator2.input_data_type).to be String
   end
 
-  it 'returns length of integer inputs' do
-    expect(@key_generator1.length).to eq(27150)
-  end
-
-  it 'returns length of string inputs' do
-    expect(@key_generator2.length).to eq(9)
-  end
-
   it 'returns random digit between 0 and 9' do
     allow(@key_generator1).to receive(:random_digit).and_return(8)
 
@@ -40,8 +32,11 @@ RSpec.describe KeyGenerator do
   end
 
   it 'returns SHA1_base64_digest hash for string' do
-    # binding.pry
     expect(@key_generator2.hash_base64).to eq("q84k4HpPRNqSDTesNapvEOhObHI=")
+  end
+
+  it 'returns first five characters of base64 hash' do
+    expect(@key_generator2.input_hash_first_5).to eq("q84k4")
   end
 
 end
