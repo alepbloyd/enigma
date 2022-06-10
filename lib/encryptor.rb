f3b914355d7518216955f1c92169ac55c8a6ad65
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-10 14:21:32 -0400

    Feat: return input message

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -4,14 +4,14 @@ require 'digest'
 
 class Encryptor
 
-  attr_reader :input_string,
+  attr_reader :input_message,
               :input_key,
               :input_date
 
-  def initialize(input_string,input_key = nil,input_date = nil)
-    @input_string = input_string
+  def initialize(input_message,input_key = nil,input_date = nil)
+    @input_message = input_message
     @input_key = input_key
     @input_date = input_date
   end
-  
+
 end
