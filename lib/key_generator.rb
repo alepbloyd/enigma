1d932cbc4df75add9b1eb0330e8665a21187a7dc
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-10 00:50:14 -0400

    Feat: random_digit

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -2,13 +2,16 @@ require 'pry'
 
 class KeyGenerator
 
+  #read an input
+  #if it's a string, convert it to a five digit hash (not random)
+  #if it's an integer of five digits, use that instead of hash
+
   attr_reader :input,
               :input_data_type
 
   def initialize(input)
     @input = input
     @input_data_type = input.class
-    # binding.pry
   end
 
   def length
@@ -17,7 +20,10 @@ class KeyGenerator
     elsif @input_data_type == String
       @input.length
     end
+  end
 
+  def random_digit
+    rand(9)
   end
 
 end
