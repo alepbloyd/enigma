acbecc0d02cd237f2aff198e42f7351b21964849
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-10 12:04:40 -0400

    Feat: first_five_ord_numbers_joined

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,6 +1,8 @@
 require 'pry'
 require 'digest'
 
+# this is a sort of self assigned extension, thought it would be interesting to see if I could have the user input take strings as passwords instead of a five digit code, and then convert password to five digit code to work with the rest of the program
+
 class KeyGenerator
 
   #read an input
@@ -9,7 +11,8 @@ class KeyGenerator
 
   attr_reader :input,
               :input_data_type,
-              :input_hash
+              :input_hash,
+              :input_hash_first_5
 
   def initialize(input)
     @input = input
@@ -28,6 +31,19 @@ class KeyGenerator
 
   def input_hash_first_5
     @input_hash_first_5 = @input_hash[0,5]
+  end
+
+  def ord_numbers_for_first_five
+    return_array = []
+    chars = input_hash_first_5.split('')
+    chars.each do |char|
+      return_array << char.ord
+    end
+    return_array
+  end
+
+  def first_five_ord_numbers_joined
+    ord_numbers_for_first_five.map {|number| number.to_s}.join.to_i
   end
 
 end
