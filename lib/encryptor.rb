b006c46b575942cecd93fb2c951fbc8bfa58a4d9
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-11 00:53:26 -0400

    Feat: a offset

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,6 +1,7 @@
 require 'key_generator'
 require 'date_generator'
 require 'a_b_c_d_keysetter'
+require 'offsetter'
 require 'enigma'
 require 'digest'
 
@@ -14,22 +15,26 @@ class Encryptor
               :b_key,
               :c_key,
               :d_key,
+              :a_offset,
               :character_set
 
   def initialize(input_message,code_key = nil,date = nil)
-    @key_gen = KeyGenerator.new(code_key)
-    @date_gen = DateGenerator.new(date)
-
     @input_message = input_message
+
+    @key_gen = KeyGenerator.new(code_key)
     @code_key = @key_gen.code_key
+
+    @date_gen = DateGenerator.new(date)
     @date = @date_gen.date
 
     @abcd_gen = ABCDKeySetter.new(@code_key)
-
     @a_key = @abcd_gen.a_key
     @b_key = @abcd_gen.b_key
     @c_key = @abcd_gen.c_key
     @d_key = @abcd_gen.d_key
+
+    @offsetter = Offsetter.new(@date)
+    @a_offset = @offsetter.a_offset
 
     @character_set = ["a", "b", "c", "d", "e",
                       "f", "g", "h", "i", "j",
