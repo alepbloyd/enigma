b830c5065f716ae52221843500964141c0df9ae0
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-10 23:28:28 -0400

    Feat: returns date

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -6,14 +6,15 @@ class Encryptor
 
   attr_reader :input_message,
               :code_key,
-              :input_date
+              :date
 
-  def initialize(input_message,code_key = nil,input_date = nil)
+  def initialize(input_message,code_key = nil,date = nil)
     key_gen = KeyGenerator.new(code_key)
+    date_gen = DateGenerator.new(date)
 
     @input_message = input_message
     @code_key = key_gen.code_key
-    @input_date = input_date
+    @date = date_gen.date
   end
 
 end
