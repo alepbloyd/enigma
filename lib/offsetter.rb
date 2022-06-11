e8b34ee1fa0b69f7e4cba65df3887ad041a6946a
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-11 00:40:56 -0400

    Feat: d offset

--- lib/offsetter.rb
+++ lib/offsetter.rb
@@ -8,13 +8,15 @@ class Offsetter
   attr_reader :squared_date,
               :a_offset,
               :b_offset,
-              :c_offset
+              :c_offset,
+              :d_offset
 
   def initialize(input)
     @squared_date = input.to_i ** 2
     @a_offset = @squared_date.to_s[-4].to_i
     @b_offset = @squared_date.to_s[-3].to_i
     @c_offset = @squared_date.to_s[-2].to_i
+    @d_offset = @squared_date.to_s[-1].to_i
   end
 
 
