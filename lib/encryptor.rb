0098115e455e6f54398d2c77be0a9a6a1fd97043
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-11 00:06:02 -0400

    Feat: abcd keys

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,5 +1,6 @@
 require 'key_generator'
 require 'date_generator'
+require 'a_b_c_d_keysetter'
 require 'enigma'
 require 'digest'
 
@@ -8,15 +9,27 @@ class Encryptor
   attr_reader :input_message,
               :code_key,
               :date,
+              :abcd_gen,
+              :a_key,
+              :b_key,
+              :c_key,
+              :d_key,
               :character_set
 
   def initialize(input_message,code_key = nil,date = nil)
-    key_gen = KeyGenerator.new(code_key)
-    date_gen = DateGenerator.new(date)
+    @key_gen = KeyGenerator.new(code_key)
+    @date_gen = DateGenerator.new(date)
 
     @input_message = input_message
-    @code_key = key_gen.code_key
-    @date = date_gen.date
+    @code_key = @key_gen.code_key
+    @date = @date_gen.date
+
+    @abcd_gen = ABCDKeySetter.new(@code_key)
+
+    @a_key = @abcd_gen.a_key
+    @b_key = @abcd_gen.b_key
+    @c_key = @abcd_gen.c_key
+    @d_key = @abcd_gen.d_key
 
     @character_set = ["a", "b", "c", "d", "e",
                       "f", "g", "h", "i", "j",
