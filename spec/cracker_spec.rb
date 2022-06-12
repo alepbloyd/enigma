31888fd7d15abc12a9d3d50a4fee541a335611fb
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-12 15:16:57 -0400

    Test: cracker exists

---
+++ spec/cracker_spec.rb
@@ -0,0 +1,25 @@
+require 'simplecov'
+SimpleCov.start
+require './lib/enigma'
+require './lib/key_generator'
+require './lib/date_generator'
+require './lib/a_b_c_d_keysetter'
+require './lib/encryptor'
+require './lib/decryptor'
+require 'digest'
+require 'pry'
+
+RSpec.describe Cracker do
+
+  before(:each) do
+    @enigma = Enigma.new
+    @encryption_hash = @enigma.encrypt("hello world end", "08304", "291018")
+
+    @cracker = Cracker.new("vjqtbeaweqihssi", "291018")
+  end
+
+  it 'exists' do
+    expect(@cracker).to be_instanceof(Cracker)
+  end
+
+end
