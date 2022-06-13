e4a7e60fe5019e9b8ed94c476d8ce381c481fa31
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-12 22:02:20 -0400

    Feat: potential_keys_array

--- lib/brute_force_cracker.rb
+++ lib/brute_force_cracker.rb
@@ -10,11 +10,24 @@ require 'pry'
 class BruteForceCracker
 
   attr_reader :input_string,
-              :date
+              :date,
+              :potential_keys
 
   def initialize(input_string, date)
     @input_string = input_string
     @date = date
+
+    @potential_keys = []
+
+    i = 0
+    until i == 100_000
+      @potential_keys << ("%05d" % i).to_s
+      i += 1
+    end
+  end
+
+  def attempt_crack(key)
+    attempt = Decryptor.new(@input_string,key,@date)
   end
 
 end
